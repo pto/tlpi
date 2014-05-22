607967791dba8b1608bfa032d8ffeb488eeea98b
Peter Olsen <pto@me.com>
2014-05-21 20:05:18 -0400

    using intmax_t

--- ch05/getrlimit.c
+++ ch05/getrlimit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 
@@ -9,17 +10,17 @@ int main()
 
 	result = getrlimit(RLIMIT_CPU, &rlim);
 	printf("RLIMIT_CPU:\n");
-	printf("current: %ld\n", (long)rlim.rlim_cur);
-	printf("max:     %ld\n", (long)rlim.rlim_max);
+	printf("current: %jd\n", (intmax_t)rlim.rlim_cur);
+	printf("max:     %jd\n", (intmax_t)rlim.rlim_max);
 
 	result = getrlimit(RLIMIT_NOFILE, &rlim);
 	printf("RLIMIT_NOFILE:\n");
-	printf("current: %ld\n", (long)rlim.rlim_cur);
-	printf("max:     %ld\n", (long)rlim.rlim_max);
+	printf("current: %jd\n", (intmax_t)rlim.rlim_cur);
+	printf("max:     %jd\n", (intmax_t)rlim.rlim_max);
 
 	result = getrlimit(RLIMIT_NPROC, &rlim);
 	printf("RLIMIT_NPROC:\n");
-	printf("current: %ld\n", (long)rlim.rlim_cur);
-	printf("max:     %ld\n", (long)rlim.rlim_max);
+	printf("current: %jd\n", (intmax_t)rlim.rlim_cur);
+	printf("max:     %jd\n", (intmax_t)rlim.rlim_max);
 
 }
