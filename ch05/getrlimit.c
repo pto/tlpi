07c53ea1a2c1058b0e1a325b534631739cad4538
Peter Olsen <pto@me.com>
2014-05-21 20:01:55 -0400

    more getrlimits

--- ch05/getrlimit.c
+++ ch05/getrlimit.c
@@ -7,7 +7,19 @@ int main()
 	int result;
 	struct rlimit rlim;
 
-	result = getrlimit(RLIMIT_NOFILE, &rlim);
+	result = getrlimit(RLIMIT_CPU, &rlim);
+	printf("RLIMIT_CPU:\n");
 	printf("current: %ld\n", (long)rlim.rlim_cur);
 	printf("max:     %ld\n", (long)rlim.rlim_max);
+
+	result = getrlimit(RLIMIT_NOFILE, &rlim);
+	printf("RLIMIT_NOFILE:\n");
+	printf("current: %ld\n", (long)rlim.rlim_cur);
+	printf("max:     %ld\n", (long)rlim.rlim_max);
+
+	result = getrlimit(RLIMIT_NPROC, &rlim);
+	printf("RLIMIT_NPROC:\n");
+	printf("current: %ld\n", (long)rlim.rlim_cur);
+	printf("max:     %ld\n", (long)rlim.rlim_max);
+
 }
