064813b9dcd4a152a443f71712112085a64319cc
Peter Olsen <pto@me.com>
2014-05-22 19:24:14 -0400

    adding page size

--- ch06/layout.c
+++ ch06/layout.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 extern char etext, edata, end;
 
@@ -15,6 +16,10 @@ int main()
 {
 	int stack;
 	void *heap = malloc(10);
+	void *program_break = sbrk(0);
+	long page_size = sysconf(_SC_PAGESIZE);
+
+	printf("page size      %#14lx (%ld)\n\n", page_size, page_size);
 
 	printf("&function      %14p\n", &function);
 	printf("&etext         %14p\n", &etext);
@@ -23,6 +28,7 @@ int main()
 	printf("&uninitialized %14p\n", &uninitialized);
 	printf("&end           %14p\n", &end);
 	printf("heap           %14p\n", heap);
+	printf("program_break  %14p\n", program_break);
 	printf("&stack         %14p\n", &stack);
 
 	free(heap);
